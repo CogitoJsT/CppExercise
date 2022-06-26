49735ca06d25cf48f23018eb01398f7395e3f989
astrophenomenon <107760709+CogitoJsT@users.noreply.github.com>
2022-06-27 01:20:40 +0900

    Update 1874_StackSequence.cpp
    Updated
    - unicode -> non-unicode
    - wcin/wcout -> scanf, printf
    
    Before
    - Memory : 4500KB
    - Time : 16ms
    - Code size : 819KB
    
    After
    - Memory : 1816KB
    - Time : 16ms
    - Code size : 966KB

--- BJCodingTest/AlgorithmBasic/1874_StackSequence.cpp
+++ BJCodingTest/AlgorithmBasic/1874_StackSequence.cpp
@@ -1,21 +1,24 @@
-#include <iostream>
+#include <cstdio>
+#include <string>
 #include <stack>
 
 int main()
 {
-    std::wcin.tie(nullptr);
-    std::ios_base::sync_with_stdio(false);
+    //std::wcin.tie(nullptr);
+    //std::ios_base::sync_with_stdio(false);
 
     int numOfSequence {0};
-    std::wcin >> numOfSequence;
+    //std::wcin >> numOfSequence;
+    scanf("%d", &numOfSequence);
 
     std::stack<int> seqStack;
-    std::wstring ans;
+    std::string ans;
     ans.reserve(numOfSequence);
     for(int i = 1, j = 1; i <= numOfSequence; ++i)
     {
         int number {0};
-        std::wcin >> number;
+        //std::wcin >> number;
+        scanf("%d", &number);
 
         int count {0};
         if(number >= j)
@@ -23,21 +26,23 @@ int main()
             while(number >= j)
             {
                 seqStack.push(j++);
-                ans.append(L"+\n");
+                ans.append("+\n");
             }
         }
 
         auto popNum = seqStack.top();
         if(popNum != number)
         {
-            std::wcout << L"NO\n";
+            //std::wcout << L"NO\n";
+            printf("NO\n");
             return 0;
         }
         seqStack.pop();
-        ans.append(L"-\n");
+        ans.append("-\n");
     }
 
-    std::wcout << ans;
+    //std::wcout << ans;
+    printf("%s", ans.c_str());
 
     return 0;
 }
