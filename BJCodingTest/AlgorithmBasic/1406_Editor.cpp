0ca7bb540c20bbf96023d0d43cca19fa7f3f131e
astrophenomenon <107760709+CogitoJsT@users.noreply.github.com>
2022-06-30 01:38:27 +0900

    Update 1406_Editor.cpp
    Change Vector -> linked list
    but it is stll time over....

--- BJCodingTest/AlgorithmBasic/1406_Editor.cpp
+++ BJCodingTest/AlgorithmBasic/1406_Editor.cpp
@@ -2,6 +2,129 @@
 #include <string>
 #include <vector>
 
+struct CNode{
+    CNode *next;
+    wchar_t data;
+    size_t size;
+};
+
+void addNode(CNode *head, wchar_t data)
+{
+    CNode *cur = head;
+
+    while(cur->next != nullptr)
+    {
+        cur = cur->next;
+    }
+
+    auto newNode = new CNode;
+    newNode->next = nullptr;
+    newNode->data = data;
+    newNode->size = 0;
+    cur->next = newNode;
+
+    head->size++;
+
+}
+
+CNode* insertNodeBefore(CNode *head, wchar_t data, size_t pos)
+{
+    if(pos > head->size)
+    {
+        return nullptr;
+    }
+
+    if(pos == head->size)
+    {
+        addNode(head, data);
+        return head;
+    }
+
+    CNode *cur = head;
+    CNode *prev = nullptr;
+    for(int i = 0; i < pos; ++i)
+    {
+        prev = cur;
+        cur = cur->next;
+    }
+
+    if(cur == head)
+    {
+        CNode *newNode = new CNode;
+        newNode->data = data;
+        newNode->next = head;
+        newNode->size = head->size + 1;
+        return newNode;
+    }
+    else
+    {
+        CNode *newNode = new CNode;
+        newNode->data = data;
+        newNode->next = cur;
+        newNode->size = 0;
+        prev->next = newNode;
+        head->size++;
+        return head;
+    }
+}
+
+CNode* deleteNode(CNode *head, size_t pos)
+{
+    if(pos > head->size - 1)
+    {
+        return nullptr;
+    }
+
+    CNode *cur = head;
+    CNode *prev = nullptr;
+    for(int i = 0; i < pos; ++i)
+    {
+        prev = cur;
+        cur = cur->next;
+    }
+
+    if(cur == head)
+    {
+        cur = head->next;
+        cur->size = head->size - 1;
+        delete head;
+        return cur;
+    }
+    else if(cur->next == nullptr)
+    {
+        prev->next = nullptr;
+        delete cur;
+        head->size--;
+        return head;
+    }
+    else
+    {
+        prev->next = cur->next;
+        delete cur;
+        head->size--;
+        return head;
+    }
+}
+
+void displayNode(CNode *head)
+{
+    CNode *cur = head;
+    auto index = 0L;
+
+    while(true)
+    {
+        std::wcout << L"//// Index " << index << L" ////\n";
+        std::wcout << L"data : " << cur->data << L"\n";
+        std::wcout << L"size : " << cur->size << L"\n";
+        cur = cur->next;
+        index++;
+
+        if(cur == nullptr)
+        {
+            break;
+        }
+    }
+}
 
 int main()
 {
@@ -12,18 +135,20 @@ int main()
     inputString.reserve(100000);
     std::wcin >> inputString;
 
-    std::vector<wchar_t> stringVector;
-    stringVector.reserve(100000);
-    for(auto ch : inputString)
+    CNode *head = new CNode;
+    head->next = nullptr;
+    head->data = L'A';
+    head->size = 1;
+    for(auto elem : inputString)
     {
-        stringVector.push_back(ch);
+        addNode(head, elem);
     }
+    head = deleteNode(head, 0);
 
     int numOfCommands{0};
     std::wcin >> numOfCommands;
 
-    //auto cursorPos = inputString.length();
-    auto cursorPos = stringVector.size();
+    auto cursorPos = head->size;
     for(int i = 0; i < numOfCommands; ++i)
     {
         wchar_t com0;
@@ -32,8 +157,7 @@ int main()
         {
             wchar_t com1;
             std::wcin >> com1;
-            //inputString.insert(cursorPos++, 1, com1);
-            stringVector.insert(stringVector.begin() + cursorPos++, com1);
+            head = insertNodeBefore(head, com1, cursorPos++);
         }
 
         if(com0 == L'L')
@@ -45,8 +169,7 @@ int main()
         }
         else if(com0 == L'D')
         {
-            //if(cursorPos < inputString.length())
-            if(cursorPos < stringVector.size())
+            if(cursorPos < head->size)
             {
                 ++cursorPos;
             }
@@ -55,18 +178,19 @@ int main()
         {
             if(cursorPos > 0)
             {
-                //inputString.erase(cursorPos-1, 1);
-                stringVector.erase(stringVector.begin() + cursorPos-1);
+                head = deleteNode(head, cursorPos-1);
                 --cursorPos;
             }
         }
     }
 
-    //std::wcout << inputString + L"\n";
-    for(auto elem : stringVector)
+    CNode *cur = head;
+    while(cur != nullptr)
     {
-        std::wcout << elem;
+        std::wcout << cur->data;
+        cur = cur->next;
     }
+
     std::wcout << L"\n";
 
     return 0;
