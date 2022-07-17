55c669ece0f667e657da530a2c17d1756f6e1ebd
astrophenomenon <107760709+CogitoJsT@users.noreply.github.com>
2022-07-17 22:28:24 +0900

    Create 10820_Stringanalysis.cpp
    Result : FAIL
    Answer is incorrect

---
+++ BJCodingTest/AlgorithmBasic/10820_Stringanalysis.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <array>
+#include <algorithm>
+#include <cctype>
+
+void init()
+{
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+}
+
+int main()
+{
+    init();
+
+    std::string inputStr;
+    std::getline(std::cin, inputStr);
+
+    std::array<int, 4> countArr{};
+
+    for (auto ch : inputStr)
+    {
+        if (std::islower(ch))
+        {
+            countArr[0]++;
+        }
+        else if (std::isupper(ch))
+        {
+            countArr[1]++;
+        }
+        else if (std::isdigit(ch))
+        {
+            countArr[2]++;
+        }
+        else if (ch == ' ')
+        {
+            countArr[3]++;
+        }
+    }
+
+    auto hasSpace{ false };
+    std::for_each(std::begin(countArr), std::end(countArr), [&hasSpace](auto elem) {
+        if (hasSpace)
+        {
+            std::cout << " ";
+        }
+        else
+        {
+            hasSpace = true;
+        }
+        std::cout << elem;
+    });
+    std::cout << "\n";
+
+   
+    return 0;
+}
