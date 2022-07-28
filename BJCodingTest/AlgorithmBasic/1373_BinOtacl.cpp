68697ab67e2a7a8da4d8cc1d0ca1dc686dab59f7
astrophenomenon <107760709+CogitoJsT@users.noreply.github.com>
2022-07-28 23:59:16 +0900

    Update 1373_BinOtacl.cpp

--- BJCodingTest/AlgorithmBasic/1373_BinOtacl.cpp
+++ BJCodingTest/AlgorithmBasic/1373_BinOtacl.cpp
@@ -46,12 +46,45 @@ int decimToOctal(int num)
     return sum;
 }
 
+void binToOct(std::string binaryNum)
+{
+    int firstOct = binaryNum.size() % 3;
+    int remainOct = binaryNum.size() / 3;
+
+    if(firstOct == 0)
+    {
+        for(int i = 0; i < remainOct; ++i)
+        {
+            std::string subStr = binaryNum.substr(0, 3);
+            auto pp = binToDecim(subStr);
+            std::cout << pp;
+            binaryNum = binaryNum.substr(3);
+        }
+    }
+    else
+    {
+        std::string subStr = binaryNum.substr(0, firstOct);
+        auto pp = binToDecim(subStr);
+        std::cout << pp;
+        binaryNum = binaryNum.substr(firstOct);
+        for(int i = 0; i < remainOct; ++i)
+        {
+            subStr = binaryNum.substr(0, 3);
+            pp = binToDecim(subStr);
+            std::cout << pp;
+            binaryNum = binaryNum.substr(3);
+        }
+    }
+    std::cout << "\n";
+}
+
 int main()
 {
     std::string numberStr;
+    numberStr.reserve(1000000);
     std::cin >> numberStr;
 
-    std::cout << decimToOctal(binToDecim(numberStr)) << "\n";
+    binToOct(numberStr);
 
     return 0;
 }
